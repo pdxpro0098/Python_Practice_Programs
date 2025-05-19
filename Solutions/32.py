4e938e5a8e9c9790395bc639de7b13acae8ccdba
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-19 23:28:14 +0530

    Update 32.py

--- Solutions/32.py
+++ Solutions/32.py
@@ -1,6 +1,7 @@
-a = [2,4,5,6,2]
-result = 0
-for i in range(0,len(a)):
-    result = result + a[i]
+def sumNatural(a = [2,4,5,6,2]):
+        result = 0
+        for i in range(0,len(a)):
+            result = result + a[i]
+        return f"The sum of all Natural No is: {result}"
 
-print(result)
+print(sumNatural(a = [2,4,5,6,2]))
