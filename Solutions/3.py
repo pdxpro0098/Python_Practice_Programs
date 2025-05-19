6c89c459b7c401aa307e336d4441277a0f0a7794
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-05-19 23:21:29 +0530

    Update 3.py
    - I made a function to find area of triangle 
    - I Used two variables heigth & base
    - return the output using fstring
    - Then i called the function

--- Solutions/3.py
+++ Solutions/3.py
@@ -1,5 +1,4 @@
 def areaTriangle(height, base):
-    area = 1/2 * (base*height)
-    print(f"The area of triangle is {area}")
+    return f"The area of triangle is {1/2*(base*height)}"
 
-areaTriangle(4,6)
+print(areaTriangle(8, 4))
