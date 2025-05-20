c6a4ed88fe013e1a8796773027c8d835cc88f607
pdxpro0098 <125691886+pdxpro0098@users.noreply.github.com>
2025-05-20 19:33:34 +0530

    Update 32.py

--- Solutions/32.py
+++ Solutions/32.py
@@ -1,7 +1,8 @@
-def sumNatural(a = [2,4,5,6,2]):
+def sumNatural(*a):
         result = 0
-        for i in range(0,len(a)):
-            result = result + a[i]
-        return f"The sum of all Natural No is: {result}"
+        for i in a:
+            result += i
+        return result
+        
 
-print(sumNatural(a = [2,4,5,6,2]))
+print(f"The sum of all Natural No is: {sumNatural(1,2,3,4,5)}")
