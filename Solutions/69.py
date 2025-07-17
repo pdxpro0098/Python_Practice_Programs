77b0d4ef638b7fa85f856d6aded56c8bf3293543
PDX PRO <pdxpro0098@gmail.com>
2025-07-17 19:22:03 +0530

    add 69.py

--- Solutions/69.py
+++ Solutions/69.py
@@ -1,2 +1,9 @@
-# 69. Write a Python Program to sort a dictionary by value.
+dictionary = {'a': 4, 'b': 2, 'c': 1, 'd': 3}
 
+
+def sortDictByValue(d):
+    return dict(sorted(d.items(), key=lambda item: item[1]))
+
+
+sorted_dict = sortDictByValue(dictionary)
+print(sorted_dict)
