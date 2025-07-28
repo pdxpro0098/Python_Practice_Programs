d916ff4ee959b145c15f049783d6be2eb7fc626d
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-07-28 18:14:35 +0530

    Replacement 52.py

--- Solutions/52.py
+++ Solutions/52.py
@@ -1,2 +1,12 @@
-lst = [15,61,99,39,12,34,56,78,90]
-print(sorted(lst)[-2])
+def findMax(x):
+    max = x[0]
+    for i in range(0,len(x)):
+        if(x[i]>max):
+            max = x[i]
+    return max
+
+items = [2,5,8,4,3]
+
+maximum= findMax(items)
+print(f"The maximum number of The list is {maximum}")
+    
