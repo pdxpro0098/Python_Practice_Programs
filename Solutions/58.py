fd2658e53c26c8e8fff0a703e3029e556181ce5b
Dalip Kumar <dalip123@gmail.com>
2025-07-30 17:36:03 +0530

    changes  58.py

--- Solutions/58.py
+++ Solutions/58.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 lst = [1, 1, 1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7, 8, 9, 9, 10, 10, 10, 10, 10]
 
 lstMap = dict()
@@ -9,3 +10,19 @@ for val in lst:
         lstMap[val] += 1
 
 print(lstMap)
+
+# Dalip's Approach
+def occurence(x,n):
+    count = 0
+    for i in x:
+        if(x[i] == n):
+            count += 1
+    return count
+
+items = [2,4,3,6,4,5,7,6,4]
+
+number = int(input("Enter the number which you want to find: "))
+
+occured = occurence(items,number)
+
+print(f"Occurence of {number} is {occured}")
