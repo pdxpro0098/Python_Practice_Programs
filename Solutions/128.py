614fe729ca14af4b4aa85e2a2fd34896eb60d540
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-08-01 10:54:27 +0530

    optimised128.py

--- Solutions/128.py
+++ Solutions/128.py
@@ -1,11 +1,11 @@
-
 def squareOfDigit(n):
     if n>0:
         c = n % 10
-        print(c,"=",c**2)
-        squareOfDigit(n//10)
-    return 1
+        # print(c,"=",c**2)
+        # squareOfDigit(n//10)
+        return [c**2] + squareOfDigit(n//10)
+    return []
 
 n = 456
 print("All Digits Squares:")
-squareOfDigit(n)
+print(squareOfDigit(n))
