4de5daf2a34d68ee98e8624c0d3fdd2f4bfd0473
pdxpro0098 <125691886+pdxpro0098@users.noreply.github.com>
2025-08-02 14:46:26 +0530

    Update 104.py

--- Solutions/104.py
+++ Solutions/104.py
@@ -1,8 +1,7 @@
 def factorial(n):
-    if(n>0):
-        n*= factorial(n-1)
-        return n
-    return 1
+    if n==0 or n==1:
+        return 1
+    return n * factorial(n-1)
 
 
 print(factorial(5))
