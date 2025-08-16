afc684b9d5dd1f93ef2dcfdf8f59af87e6fa0ec9
PDX PRO <pdxpro0098@gmail.com>
2025-08-16 15:33:53 +0530

    add 102.py

---
+++ Solutions/102.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def add(x, y):
+        return x + y
+
+    def sub(x, y):
+        return x - y
+
+    def mul(x, y):
+        return x * y
+
+    def div(x, y):
+        return x / y
+
+
+print(Calculator.add(10, 2))
+print(Calculator.sub(10, 2))
+print(Calculator.mul(10, 2))
+print(Calculator.div(10, 2))
