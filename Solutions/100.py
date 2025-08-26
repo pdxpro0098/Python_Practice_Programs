efcc9d9bd51bc56646bdb94b4877998f5ff797ef
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-08-26 15:56:17 +0530

    Update 100.py

--- Solutions/100.py
+++ Solutions/100.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 class myClass():
     def __init__(self, name,age):
         self.name = name
@@ -6,3 +7,14 @@ class myClass():
 cls = myClass("krish",20)
 
 print(cls.__dict__)
+
+# Dalip's Approach
+class Student:
+    def __init__(self,name,roll_no,):
+        self.name = name
+        self.roll_no = roll_no
+
+s1 = Student("DalipKumar",456)
+
+print(f"Name: {s1.name}")
+print(f"Roll No: {s1.roll_no}")
