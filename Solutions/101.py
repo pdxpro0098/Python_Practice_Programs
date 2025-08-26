030768cd59e703e58a4de9d8ce38b181e0dea281
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-08-26 16:49:32 +0530

    Update 101.py

--- Solutions/101.py
+++ Solutions/101.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 class Circle:
     def __init__(self, radius):
         self.radius = radius
@@ -8,4 +9,18 @@ class Circle:
 
 c = Circle(12)
 area = c.area()
+print(area)
+
+# Dalip's Approach
+class circle:
+    def __init__(self,radius):
+        self.radius= radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+    
+
+c1 = circle(8)
+
+area = c1.area()
 print(area)
