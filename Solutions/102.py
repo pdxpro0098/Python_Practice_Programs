b6a04960f80e5729443c05caac8992cf9fed05c1
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-08-26 17:20:02 +0530

    Update 102.py

--- Solutions/102.py
+++ Solutions/102.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 class Calculator:
     def add(x, y):
         return x + y
@@ -16,3 +17,24 @@ print(Calculator.add(10, 2))
 print(Calculator.sub(10, 2))
 print(Calculator.mul(10, 2))
 print(Calculator.div(10, 2))
+
+
+# Dalip's Approach
+class Calculator:
+    
+    def add(a,b):
+        return a + b
+
+    def subtract(a,b):
+        return a - b
+
+    def product(a,b):
+        return a * b
+
+    def divide(a,b):
+        if(b == 0):
+            return "Error! Division by zero."
+        return a / b
+
+
+print("Addition:",Calculator.add(3,5))
