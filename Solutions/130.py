9e8bf91409da74211ab0ed69ad80f381d988015c
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-09-05 16:46:32 +0530

    changes130.py

--- Solutions/130.py
+++ Solutions/130.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 def mean(nums):
     lst = [int(digit) for digit in str(nums)]
     m = 0
@@ -7,6 +8,22 @@ def mean(nums):
         m += (num)
     return m / c
 
-
 digit = 4444
 print(mean(digit))
+
+
+# Dalip's Approach
+digit = 352
+def findMean(digit):
+    addition = 0
+    count = 0
+    while(digit>0):
+        c = digit %10
+        count +=1
+        addition += c
+        digit //= 10
+
+    return addition / count
+
+print("Mean:",format(findMean(digit),".2f"))
+
