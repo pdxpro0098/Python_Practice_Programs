1cd0fdf2d143c01a95c14f5eda2b9e7cf94a1eb7
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-09-07 14:35:49 +0530

    changes134.py

--- Solutions/134.py
+++ Solutions/134.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 with open("temp.txt", "r") as file:
     allWords = file.read()
     
@@ -10,3 +11,18 @@ def countWord(string):
 
 
 print(countWord(allWords))
+
+# Dalip's Approach
+def  cout_word_in_file(fileName):
+    try:
+        with open(fileName,"r") as file:
+            text = file.read()
+            words = text.split()
+            return len(words)
+    except FileNotFoundError:
+        print("File not found!")
+        return 0
+    
+fileName = 'dalip.txt'
+word_count = cout_word_in_file(fileName)
+print(f"Total words in '{fileName}': {word_count} ")
