bab4d6e397a66dc59fbfdab3fd6480979bec418a
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-09-12 13:53:03 +0530

    changes133.py

--- Solutions/133.py
+++ Solutions/133.py
@@ -1,3 +1,4 @@
+# krish's Approach
 b = 10010110010
 
 
@@ -10,3 +11,29 @@ def countSetBit(number):
 
 
 print(countSetBit(b))
+
+
+# Dalip's Approach
+
+def decimalToBinary(decimalNum):
+    power = 1
+    ans = 0
+    while(decimalNum > 0):
+        remainder = decimalNum % 2 
+        decimalNum //= 2
+        ans += (remainder*power)
+        power = power * 10
+    return ans
+
+
+
+def countSetBits(n):
+    count = 0
+    while(n>0):
+        c = n % 10
+        if(c == 1):
+            count += 1
+        n//=10
+    return count
+
+print(f"Set bits: {countSetBits(decimalToBinary(15))}")
