5dc40476271e5d5716e192847594cf5262475b88
dalipkumar4 <dalipkumar3559@gmail.com>
2025-09-15 14:00:00 +0530

    Add 98.py

--- Solutions/98.py
+++ Solutions/98.py
@@ -1,5 +1,22 @@
+# Krish's Approach
 sentence = "Write_a_Python_Program_to_convert_snake_case_to_camelCase"
+
+
 def snakeToCamel(sentence):
     return sentence.replace("_", " ")
 
+
 print(snakeToCamel(sentence))
+
+
+# Dalip's Approach
+import re
+
+
+def snakeToCamel(t):
+    camel = re.sub(r"_([a-z])", lambda m: m.group(1).upper(), text)
+    return camels
+
+
+text = "dalip_kumar"
+print(f"Snake Case : {text}\nCamel Case: {snakeToCamel(text)}")
