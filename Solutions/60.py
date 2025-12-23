5f1869d240f07596b659fdaf3c1ec851b5b300ab
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-12-23 11:31:08 +0530

    Implement multiple approaches to remove characters
    Added a second approach to remove characters from a string.

--- Solutions/60.py
+++ Solutions/60.py
@@ -1,5 +1,15 @@
+# krish's Approach
 string = "remove nth character"
 nth_char = 5
 string = string[:nth_char] + string[nth_char+1:]
 
 print(string)
+
+# Dalip's Approach
+name = "Bruno Mars"
+
+dontneed = 'o'
+
+replaced = name.replace(dontneed,"")
+
+print(replaced)
