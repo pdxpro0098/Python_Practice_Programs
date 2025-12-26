dcf8396bd746cb02bf933a70e6b192c64d311bbe
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-12-26 19:53:11 +0530

    Implement second approach for uncommon words
    Added a second approach to find uncommon words from two strings.

--- Solutions/63.py
+++ Solutions/63.py
@@ -1,3 +1,4 @@
+# krish's Approach
 str1 = "Write a Python program to find uncommon words from two strings"
 str2 = "Write a Python Program to sort a dictionary by val two strings"
 
@@ -9,3 +10,15 @@ small = min(len(lst1), len(lst2))
 for i in range(small):
     if lst1[i] != lst2[i]:
         print(lst1[i], lst2[i])
+
+# Dalip's Approach
+a = "save us from Titans"
+b = "save us from Eren"
+
+catalogue1 = a.upper().split()
+catalogue2 = b.upper().split()
+
+for i in range(0,len(catalogue1)):
+    if catalogue1[i] != catalogue2[i]:
+        print(catalogue1[i],catalogue2[i])
+        
