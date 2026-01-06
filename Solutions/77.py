d0ff07d8d032014adb8d3515f3ea99f88ae7a79f
dalipkumar4 <dalipkumar3559@gmail.com>
2026-01-06 14:00:00 +0530

    Added 77.py

--- Solutions/77.py
+++ Solutions/77.py
@@ -1,10 +1,24 @@
+# Krish's Approach
 string = "Write a Python Program to count the number of each character in a string"
 wordMap = dict()
 
 for char in string:
     if char in wordMap:
-        wordMap[char] +=1
+        wordMap[char] += 1
     else:
         wordMap[char] = 1
 
 print(wordMap)
+
+# Dalip's Approach
+from collections import defaultdict
+
+name = list("programming")
+
+count = 1
+chars = {}
+
+chars = defaultdict(int)
+for ch in name:
+    chars[ch] += 1
+print(chars)
