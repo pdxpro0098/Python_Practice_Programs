66cfc46d28734605043c0b48031a1b82e918d8ba
dalipkumar4 <dalipkumar3559@gmail.com>
2026-01-12 10:00:00 +0530

    Solved problem 53

--- Solutions/53.py
+++ Solutions/53.py
@@ -2,12 +2,15 @@
 
 lst = [121, 2, 34, 65, 81, 45, 31, 92, 69, 11]
 
+
 def nthLargest(num, list):
     if num > len(list):
         return -1
-    return sorted(lst)[num-1]
+    return sorted(lst)[num - 1]
+
 
 print(nthLargest(3, lst))
+
 
 # Dalip's Approach
 numbers = [3, 2, 5, 7]
@@ -16,3 +19,21 @@ numbers.sort(reverse=True)
 n = int(input("Enter the nth largest number: "))
 
 print(f"{n} largest element: {numbers[n-1]}")
+
+# # Dalip's Approach
+# numbers = [3, 8, 5, 7]
+
+
+# def findSecondLargestNum(numbers):
+#     largest = second = float("-inf")
+#     for num in numbers:
+#         if num > largest:
+#             second = largest
+#             largest = num
+#         elif num > second and num != largest:
+#             second = num
+#     return second
+
+
+# print(f"Second largest number: {findSecondLargestNum(numbers)}")
+
