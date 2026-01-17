51063220d57985dea81d80b0a603dfdf9abc6fd9
Dalip-Kumar <dalipkumar3559@gmail.com>
2026-01-17 22:04:42 +0530

    Implement sorting dictionary by value with two approaches
    Added Dalip's approach for sorting a dictionary by value.

--- Solutions/69.py
+++ Solutions/69.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 dictionary = {'a': 4, 'b': 2, 'c': 1, 'd': 3}
 
 
@@ -7,3 +8,9 @@ def sortDictByValue(d):
 
 sorted_dict = sortDictByValue(dictionary)
 print(sorted_dict)
+
+# Dalip's Approach
+my_dict = {"dalipkumar":3,"krish":5,"jeff":2,"elon":1}
+
+res =dict(sorted(my_dict.items(), key=lambda item: item[1]))
+print(res)
