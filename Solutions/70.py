d29be92aac617cfd4f0052b36f1aa07d7dac429f
Dalip-Kumar <dalipkumar3559@gmail.com>
2026-01-18 10:36:56 +0530

    Implement sum function for dictionary elements
    Added a function to sum dictionary values and a new dictionary for testing.

--- Solutions/70.py
+++ Solutions/70.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 dictionary = {'a':1,'b':2,'c':3} 
 
 keySum  = 0
@@ -5,3 +6,15 @@ for key,value in dictionary.items():
     keySum += value
 
 print(keySum)
+
+# Dalip's Approach
+def sumOfAllDictElements(my_dict):
+    sum = 0
+    for num in my_dict.values():
+        sum += num
+    return sum
+
+my_dict = {"dalip":3,"krish":2,"elon":1}
+
+print(f"Sum of All Elements: {sumOfAllDictElements(my_dict)}")
+    
