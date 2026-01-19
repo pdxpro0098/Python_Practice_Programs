35cf5eb730a6956085f69b6cbf03d2852b2fb619
Dalip-Kumar <dalipkumar3559@gmail.com>
2026-01-19 22:35:34 +0530

    Implement product calculation for dictionary values
    Added a new function to calculate the product of all dictionary elements.

--- Solutions/71.py
+++ Solutions/71.py
@@ -1,3 +1,4 @@
+# Krish's Approach
 dictionary = {'a': 1, 'b': 2, 'c': 3}
 
 keySum = 1
@@ -5,3 +6,15 @@ for key, value in dictionary.items():
     keySum *= value
 
 print(keySum)
+
+# Dalip's Approach
+def ProdOfAllDictElements(my_dict):
+    product = 1
+    for num in my_dict.values():
+        product *= num
+    return product
+
+my_dict = {"dalip":4,"krish":3,"elon":2}
+
+print(f"Product of All Elements: {ProdOfAllDictElements(my_dict)}")
+    
