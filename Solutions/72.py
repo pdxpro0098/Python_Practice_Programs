56ed9049eda537526537698dd46d3c251e2a1ae0
Dalip-Kumar <dalipkumar3559@gmail.com>
2026-01-20 09:03:41 +0530

    Add dictionary manipulations for Krish and Dalip

--- Solutions/72.py
+++ Solutions/72.py
@@ -1,5 +1,12 @@
+# Krish's Approach
 dictionary = {'a': 1,'b':2,'c':3} 
 
 dictionary.popitem()
 
 print(dictionary)
+
+# Dalip's Approach
+catalouge = {24:"Elephants",56:"Leopard",79:"Ostrich"}
+
+catalouge.pop(24,None)
+print(catalouge)
