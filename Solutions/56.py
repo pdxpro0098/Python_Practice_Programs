eea6a519f0cecac777a49064f2c314df0b09cff5
Dalip-Kumar <dalipkumar3559@gmail.com>
2026-01-26 07:27:18 -0800

    Implement methods to remove empty lists from a list
    Added two approaches for removing empty lists from a list.

--- Solutions/56.py
+++ Solutions/56.py
@@ -1,3 +1,4 @@
+# Krish's Appoach
 lst = [12, 90, 14, [], 45, 6, [], 12, 14, [], [0], []]
 
 for item in lst:
@@ -5,3 +6,14 @@ for item in lst:
         lst.remove(item)
 
 print(lst)
+
+
+# Dalip's Approach
+def removeEmptyList(num):     
+    for item in num:
+        if len(item) == 0:
+            num.remove(item)
+    return num
+
+num = [[2,5],[],[9,8]]
+print(f"After removing empty one: {removeEmptyList(num)}")
