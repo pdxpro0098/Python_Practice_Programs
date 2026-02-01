57f8b49067a52bf4bd30863368b948d7ff3a2983
Dalip-Kumar <dalipkumar3559@gmail.com>
2026-02-01 14:03:36 +0530

    Add compression and decompression examples using zlib

--- Solutions/84.py
+++ Solutions/84.py
@@ -1,6 +1,20 @@
+# Krish's Approach
 import zlib
 data = b'compress me'
 compressed = zlib.compress(data)
 print("Compressed:", compressed)
 decompressed = zlib.decompress(compressed)
 print("Decompressed:", decompressed)
+
+# Dalip's Approach
+import zlib;
+
+data = b"abbccceedd3a"
+
+compressed_data = zlib.compress(data)
+print(f"Original Data size: {len(data)}")
+print(f"Compressed Data size: {len(compressed_data)}")
+print(f"Compressed Data: {compressed_data}")
+
+decompressed_data = zlib.decompress(compressed_data)
+print(f"Decompressed Data: {decompressed_data}")
