895d59f47c9ec605725d5b3b6a4a26be85f96d27
dalipkumar4 <dalipkumar3559@gmail.com>
2026-03-21 14:00:00 +0530

    Problem 109 solved

--- Solutions/111.py
+++ Solutions/111.py
@@ -1,6 +1,13 @@
+# Krish's Approach
 lst = ["krish", 1, [], 5.6, "dilip", 0]
 for item in lst:
     if not isinstance(item, int):
         lst.remove(item)
 
 print(lst)
+
+# Dalip's Approach
+userName = ["d", "a", "l", "i", "p", "2", "3", "4"]
+
+newlist = list(filter(str.isdigit, userName))
+print(newlist)
